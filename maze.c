3d7671f421423af89266d39ff14c510069120528
Leonardo <leonardo.alves.melo.1995@gmail.com>
2016-05-06 10:57:49 -0300

    Acrescentei a inicialização do 3pi e seus sensores

--- maze.c
+++ maze.c
@@ -4,6 +4,25 @@
 * O Robô percorrerá o labirinto salvando seus erros e acertos, ao final, quando colocado novamente ele já saberá o menor cmainho para saida 
 * Autor: Leonardo Alves de Melo */
 
+/* Biblioteca padrao do 3pi */
+#include <pololu/3pi.h>
+
+/* Outras libs */
+#include <avr/pgmspace.h>
+#include "bargraph.h"
+#include "maze-solve.h"
+
+/* Sera printado na tela LCD ao iniciar o programa*/
+const char welcome_line1[] PROGMEM = " GER";
+const char welcome_line2[] PROGMEM = "Shannon";
+const char demo_name_line1[] PROGMEM = "Maze";
+const char demo_name_line2[] PROGMEM = "solver";
+
+/* Musicas do robo */
+const char welcome[] PROGMEM = ">g32>>c32";
+const char go[] PROGMEM = "L16 cdegreg4";
+
+
 /* Definimos a direcao do robo */
 #define NORTE 0
 #define LESTE 1
@@ -56,6 +75,103 @@ Mapa local_saida;
 
 /* Variavel que guarda a orientacao do 3pi */
 int orientacao = ORIENTACAO_INICIAL;
+
+/* Inicializa o robo, mostra uma mensagem, calibra os sensores e toca uma musica */
+/* Este codigo eh do 3pi */
+void inicializa() {
+
+	unsigned int counter; // used as a simple timer
+	unsigned int sensors[5]; // an array to hold sensor values
+
+	// This must be called at the beginning of 3pi code, to set up the
+	// sensors.  We use a value of 2000 for the timeout, which
+	// corresponds to 2000*0.4 us = 0.8 ms on our 20 MHz processor.
+	pololu_3pi_init(2000);
+	load_custom_characters(); // load the custom characters
+	
+	// Play welcome music and display a message
+	print_from_program_space(welcome_line1);
+	lcd_goto_xy(0,1);
+	print_from_program_space(welcome_line2);
+	play_from_program_space(welcome);
+	delay_ms(1000);
+
+	clear();
+	print_from_program_space(demo_name_line1);
+	lcd_goto_xy(0,1);
+	print_from_program_space(demo_name_line2);
+	delay_ms(1000);
+
+	// Display battery voltage and wait for button press
+	while(!button_is_pressed(BUTTON_B))
+	{
+		int bat = read_battery_millivolts();
+
+		clear();
+		print_long(bat);
+		print("mV");
+		lcd_goto_xy(0,1);
+		print("Press B");
+
+		delay_ms(100);
+	}
+
+	// Always wait for the button to be released so that 3pi doesn't
+	// start moving until your hand is away from it.
+	wait_for_button_release(BUTTON_B);
+	delay_ms(1000);
+
+	// Auto-calibration: turn right and left while calibrating the
+	// sensors.
+	for(counter=0;counter<80;counter++)
+	{
+		if(counter < 20 || counter >= 60)
+			set_motors(40,-40);
+		else
+			set_motors(-40,40);
+
+		// This function records a set of sensor readings and keeps
+		// track of the minimum and maximum values encountered.  The
+		// IR_EMITTERS_ON argument means that the IR LEDs will be
+		// turned on during the reading, which is usually what you
+		// want.
+		calibrate_line_sensors(IR_EMITTERS_ON);
+
+		// Since our counter runs to 80, the total delay will be
+		// 80*20 = 1600 ms.
+		delay_ms(20);
+	}
+	set_motors(0,0);
+
+	// Display calibrated values as a bar graph.
+	while(!button_is_pressed(BUTTON_B))
+	{
+		// Read the sensor values and get the position measurement.
+		unsigned int position = read_line(sensors,IR_EMITTERS_ON);
+
+		// Display the position measurement, which will go from 0
+		// (when the leftmost sensor is over the line) to 4000 (when
+		// the rightmost sensor is over the line) on the 3pi, along
+		// with a bar graph of the sensor readings.  This allows you
+		// to make sure the robot is ready to go.
+		clear();
+		print_long(position);
+		lcd_goto_xy(0,1);
+		display_readings(sensors);
+
+		delay_ms(100);
+	}
+	wait_for_button_release(BUTTON_B);
+
+	clear();
+
+	print("GER!");		
+
+	// Play music and wait for it to finish before we start driving.
+	play_from_program_space(go);
+	while(is_playing());
+
+}
 
 /* Inicializa a posicao criada */
 void inicializa_posicao(Percurso *posicao, Percurso *anterior) {
@@ -270,7 +386,11 @@ void testa_saida() {
 	}
 }
 
-void setup() {
+int main() {
+
+
+	/* Inicializa o 3pi */
+	inicializa();
 
 	/* Inicializa a posicao inicial */
 	inicializa_posicao(&posicao_inicial, NULL);
@@ -278,11 +398,9 @@ void setup() {
 
 	inicializa_mapa();
 
-
-
-}
-
-void loop() {
-
+	/* Começa o algoritmo */
 	testa_saida();
+
+	return 0;
 }
+
